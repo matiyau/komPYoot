6d502d3cd38ddd452e29b9431f524aaca37b2621
Nishad Mandlik <mandliksg@gmail.com>
2022-08-16 00:13:32 +0200

    Updated Version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="komPYoot",  # Replace with your own username
-    version="1.0.0",
+    version="1.0.1",
     author="Nishad Mandlik",
     author_email="mandliksg@gmail.com",
     description="Python Interface for Komoot API",
