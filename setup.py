5579cd9e699455218613bf78d81e8f5a588ce095
Nishad Mandlik <mandliksg@gmail.com>
2022-08-26 18:40:26 +0200

    Fixed Version Number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="komPYoot",  # Replace with your own username
-    version="1.0.1",
+    version="1.0.3",
     author="Nishad Mandlik",
     author_email="mandliksg@gmail.com",
     description="Python Interface for Komoot API",
